284d068fc2713df0acb10b00a77d2f963808dc20
Aaryav Mitra <aaryav.mitra@gmail.com>
2025-07-04 16:44:02 +0530

    Add files via upload

---
+++ one.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=12;
+    string n="Aaryav";
+    char s='C';
+    int c=7;
+    cout<<"Hello I am "<<n<<".\n I study in class "<<c<<s<<endl<<". I am "<<a<<" years old.";
+    return 0;
+}
